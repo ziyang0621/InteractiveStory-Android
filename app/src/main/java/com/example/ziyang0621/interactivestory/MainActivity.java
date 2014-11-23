c74349c02a94ebf864716c01e0e28ec467df8f09
ziyang <ziyang0621@gmail.com>
2014-11-22 21:46:00 -0800

    project update

--- app/src/main/java/com/example/ziyang0621/interactivestory/MainActivity.java
+++ app/src/main/java/com/example/ziyang0621/interactivestory/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.ziyang0621.interactivestory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -26,9 +26,14 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory(name);
             }
         });
     }
 
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/ziyang0621/interactivestory/StoryActivity.java
@@ -0,0 +1,23 @@
+package com.example.ziyang0621.interactivestory;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class StoryActivity extends Activity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        Log.d(TAG, name);
+    }
+
+}
